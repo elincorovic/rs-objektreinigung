d239fa296586127476e0f783f35158307911e2a0
Elin Corovic <ec024@hdm-stuttgart.de>
2024-01-21 03:19:09 +0100

    initial commit2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   base: "/rs-objektreinigung/",
    plugins: [vue()],
    resolve: {
       alias: {
